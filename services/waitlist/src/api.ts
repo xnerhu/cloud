6345207f227b6c8d23da1edb01a5fd02f9a7de02
Mikołaj Palkiewicz <xnerhu@gmail.com>
2021-07-22 18:00:46 +0200

    feat: waitlist redirect (#4)

---
+++ services/waitlist/src/api.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from 'fastify';
+
+export default (app: FastifyInstance) => {
+  app.get('/waitlist', (req, res) => {
+    res.redirect('https://zjyparvu55g.typeform.com/to/E9YoNwYz');
+  });
+};
