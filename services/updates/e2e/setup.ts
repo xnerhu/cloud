639d49992b4fe8205b80f5d74a0357fb962ae305
xnerhu <xnerhu@gmail.com>
2021-09-25 22:38:27 +0200

    refactor: mikro orm in e2e

--- services/updates/e2e/setup.ts
+++ services/updates/e2e/setup.ts
@@ -1,18 +1,40 @@
 import { resolve } from "path";
 import execa from "execa";
 
-const main = async () => {
-  await execa(
-    resolve(__dirname, "../schema_update.sh"),
-    ["schema:update", "--run", "--fk-checks"],
-    {
+const mikroORM = (cwd: string, _config?: string) => {
+  const config = _config
+    ? `${_config}/src/mikro-orm-config.js`
+    : process.env.MIKRO_ORM_CLI;
+
+  if (!config) throw new Error("Config file not specified");
+
+  const run = (file: string, args: string[]) => {
+    return execa(resolve(__dirname, file), args, {
       stderr: process.stderr,
       stdout: process.stdout,
       env: {
-        MIKRO_ORM_CLI: "services/updates/src/mikro-orm-config.js",
+        MIKRO_ORM_CLI: `${config}/src/mikro-orm-config.js`,
       },
-    },
-  );
+    });
+  };
+
+  const DEFAULT_ARGS = ["--run", "--fk-checks"];
+
+  return {
+    up: () => run("../schema_up.sh", ["schema:update", ...DEFAULT_ARGS]),
+    drop: () =>
+      run("../schema_drop.sh", [
+        "schema:drop",
+        "--drop-migrations-table",
+        ...DEFAULT_ARGS,
+      ]),
+  };
+};
+
+const main = async () => {
+  const { up } = mikroORM(resolve(__dirname), "services/updates");
+
+  await up();
 };
 
 export default main;
