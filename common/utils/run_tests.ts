4fd6df2b6829287fcdc824bd9aed8ce049372c53
xnerhu <xnerhu@gmail.com>
2021-10-09 15:35:14 +0200

    test

--- common/utils/run_tests.ts
+++ common/utils/run_tests.ts
@@ -11,13 +11,22 @@ const main = async () => {
     const res = await execa(testPath, []);
 
     process.stdout.write(res.stdout);
-    const env = { CI: true };
 
-    const covRes = await execa(covPath);
+    process.stdout.write("XDDD" + isCI);
 
-    process.stdout.write(covRes.stdout);
+    // const covRes = await execa(
+    //   covPath,
+    //   [
+    //     "--token=a6196374-3614-4054-94f8-4d0503ee66f4",
+    //     "--commit=1a2ad05cc6be3e4814309a3d662b61a242dd2088",
+    //     "--slug=wexond/cloud",
+    //     "--branch=updates-v2",
+    //     "--disable=detect",
+    //   ],
+    //   { env: { CI: "true" } },
+    // );
 
-    // process.stdout.write(JSON.stringify(readdirSync(resolve(__dirname))));
+    // process.stdout.write(covRes.stdout);
   } catch (error) {
     process.stderr.write(error.stderr);
     process.stdout.write(error.stdout);
