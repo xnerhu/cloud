67fbe0b004d958ab9bbe640ab5f94e17645fe730
xnerhu <xnerhu@gmail.com>
2021-10-09 21:57:44 +0200

    test

--- rules/get_workspace_status.sh
+++ rules/get_workspace_status.sh
@@ -10,30 +10,11 @@
 # If the script exits with non-zero code, it's considered as a failure
 # and the output will be discarded.
 
-# The code below presents an implementation that works for git repository
-# git_rev=$(git rev-parse HEAD)
-# if [[ $? != 0 ]];
-# then
-#     exit 1
-# fi
-# echo "BUILD_SCM_REVISION ${git_rev}"
+echo "CI true"
 
-# # Check whether there are any uncommitted changes
-# git diff-index --quiet HEAD --
-# if [[ $? == 0 ]];
-# then
-#     tree_status="Clean"
-# else
-#     tree_status="Modified"
-# fi
-# echo "BUILD_SCM_STATUS ${tree_status}"
+echo "GITHUB_HEAD_REF ${GITHUB_HEAD_REF}"
+echo "GITHUB_RUN_ID ${GITHUB_RUN_ID}"
+echo "GITHUB_SHA ${GITHUB_SHA}"
+echo "GITHUB_REPOSITORY ${GITHUB_REPOSITORY}"
 
-# echo "xd"
-
-# git_branch=$(git rev-parse --abbrev-ref HEAD || hg branch)
-# git_head=$(git log -1 --pretty=%H || hg id -i --debug | tr -d '+')
-
-# echo "GIT_BRANCH ${git_branch}"
-# echo "GIT_HEAD ${git_head}"
-
-echo "XD ${GITHUB_HEAD_REF}"
+echo "CODECOV_TOKEN ${CODECOV_TOKEN}"
--- rules/run_tests.ts
+++ rules/run_tests.ts
@@ -3,7 +3,29 @@ import { resolve } from "path";
 import { isCI } from "ci-info";
 import { readFile } from "fs/promises";
 
-// console.log();
+const parseGitStatus = (data: string) => {
+  const lines = data.split("\n");
+  const map = new Map<string, string>();
+
+  lines.forEach((line) => {
+    const [key, value] = line.split(" ");
+    map.set(key, value);
+  });
+
+  return map;
+};
+
+const getCodeCovParams = (info: Map<string, string>) => {
+  const params = {
+    branch:
+      info.get("GITHUB_HEAD_REF") ||
+      process.env.GITHUB_HEAD_REF ||
+      process.env.GITHUB_REF?.replace("refs/heads/", ""),
+    build: info.get("GITHUB_RUN_ID") || process.env.GITHUB_RUN_ID,
+    commit: info.get("GITHUB_SHA") || process.env.GITHUB_SHA,
+    slug: info.get("GITHUB_REPOSITORY") || process.env.GITHUB_REPOSITORY,
+  };
+};
 
 const main = async () => {
   const packageName = process.argv[2];
@@ -19,27 +41,17 @@ const main = async () => {
   const covPath = resolve(workingDir, "test_codecov.sh");
 
   const gitStatus = await readFile(statusPath, "utf8");
+  const gitInfo = parseGitStatus(gitStatus);
 
-  process.stdout.write(gitStatus);
+  const params = getCodeCovParams(gitInfo);
 
-  return;
+  console.log(params);
 
   try {
     const res = await execa(testPath, []);
 
     process.stdout.write(res.stdout);
     process.stderr.write(res.stderr);
-
-    const params = {
-      branch:
-        process.env.GITHUB_HEAD_REF ||
-        process.env.GITHUB_REF?.replace("refs/heads/", ""),
-      build: process.env.GITHUB_RUN_ID,
-      commit: process.env.GITHUB_SHA,
-      service: "github-actions",
-      slug: process.env.GITHUB_REPOSITORY,
-      pr: "",
-    };
 
     // console.log("xdd");
 
