80f3978e7e1133c82396aeeda28ff0d9943832a2
xnerhu <xnerhu@gmail.com>
2021-10-09 15:53:07 +0200

    test

--- common/utils/run_tests.ts
+++ common/utils/run_tests.ts
@@ -10,9 +10,24 @@ const main = async () => {
   try {
     const res = await execa(testPath, []);
 
+    const params = {
+      branch:
+        process.env.GITHUB_HEAD_REF ||
+        process.env.GITHUB_REF.replace("refs/heads/", ""),
+      build: process.env.GITHUB_RUN_ID,
+      commit: process.env.GITHUB_SHA,
+      service: "github-actions",
+      slug: process.env.GITHUB_REPOSITORY,
+    };
+
+    if (process.env.GITHUB_HEAD_REF) {
+      // PR refs are in the format: refs/pull/7/merge for pull_request events
+      params["pr"] = process.env.GITHUB_REF.split("/")[2];
+    }
+
     process.stdout.write(res.stdout);
 
-    process.stdout.write("XDDD" + process.env.GIT_BRANCH + process.env.GIT_SHA);
+    process.stdout.write("XDDD" + JSON.stringify(params));
 
     // const covRes = await execa(
     //   covPath,
