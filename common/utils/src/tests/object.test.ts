1a2ad05cc6be3e4814309a3d662b61a242dd2088
xnerhu <xnerhu@gmail.com>
2021-10-09 14:20:19 +0200

    test

--- common/utils/src/tests/object.test.ts
+++ common/utils/src/tests/object.test.ts
@@ -12,7 +12,7 @@ describe("@common/utils object", () => {
           c: 1024,
           d: null,
           e: undefined,
-          f: "test", // test
+          f: "test", // testdddddd
         }),
       ).toEqual({
         a: true,
