384b45a1c2f1410d86f51e9aa71b396a5b5bd6ac
xnerhu <xnerhu@gmail.com>
2021-10-13 16:35:12 +0200

    fix: ci

--- tools/buildstamp/get_workspace_status.sh
+++ tools/buildstamp/get_workspace_status.sh
@@ -10,13 +10,10 @@
 # If the script exits with non-zero code, it's considered as a failure
 # and the output will be discarded.
 
-git_sha=$(git log -1 --pretty=%H || hg id -i --debug | tr -d '+')
-
 echo "CI true"
 echo "CODECOV_TOKEN ${CODECOV_TOKEN}"
 echo "GITHUB_HEAD_REF ${GITHUB_HEAD_REF}"
 echo "GITHUB_REF ${GITHUB_REF}"
 echo "GITHUB_RUN_ID ${GITHUB_RUN_ID}"
-echo "GITHUB_SHA ${git_sha}"
-echo "LMAO ${GIT_PR_SHA}"
+echo "GITHUB_SHA ${GIT_PR_SHA}"
 echo "GITHUB_REPOSITORY ${GITHUB_REPOSITORY}"
--- tools/tests/run_tests.ts
+++ tools/tests/run_tests.ts
@@ -32,9 +32,6 @@ const main = async () => {
 
       const pr = status["GITHUB_REF"].split("/")[2];
 
-      process.stdout.write("XDDDD" + JSON.stringify(status) + ", " + pr);
-      process.exit(1);
-
       const covRes = await execa(covPath, [
         `--token=${status["CODECOV_TOKEN"]}`,
         `--commit=${status["GITHUB_SHA"]}`,
