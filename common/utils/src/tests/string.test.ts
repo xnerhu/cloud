f78f5cd90a525be3fe92b75a0a8450dee9b752ac
xnerhu <xnerhu@gmail.com>
2021-10-15 12:35:13 +0200

    test: add mising test for string utils

--- common/utils/src/tests/string.test.ts
+++ common/utils/src/tests/string.test.ts
@@ -1,11 +1,21 @@
 import "jest";
 
-import { makeId } from "../string";
+import { makeId, capitalizeFirstLetter } from "../string";
 
 describe("@common/utils string", () => {
   describe("makeId", () => {
     it("returns a random string", () => {
       expect(makeId(12)).toHaveLength(12);
+    });
+  });
+
+  describe("capitalizeFirstLetter", () => {
+    it("returns first upper case letter and rest lower case", () => {
+      expect(capitalizeFirstLetter("aBC")).toEqual("Abc");
+    });
+
+    it("returns the same string", () => {
+      expect(capitalizeFirstLetter("Abc")).toEqual("Abc");
     });
   });
 });
