0aaf821befcd7da51549112fd2aa91b7cbcb88df
xnerhu <xnerhu@gmail.com>
2021-10-23 23:08:14 +0200

    fix: handle asset extensions

--- services/updates/src/assets/assets-utils.ts
+++ services/updates/src/assets/assets-utils.ts
@@ -1,5 +1,6 @@
 import { BadRequestException } from "@nestjs/common";
 import hashFile from "md5-file";
+import { extname } from "path";
 import { AssetType } from "@core/updates";
 
 export const assetTypeToString = (type: AssetType) => {
@@ -20,4 +21,20 @@ export const verifyAssetHash = async (
       )} is corrupt. Expected ${remoteHash}, got: ${localHash}`,
     );
   }
+};
+
+export const getAssetExtension = (path: string, type: AssetType) => {
+  if (type === AssetType.INSTALLER) {
+    return extname(path);
+  }
+
+  if (type === AssetType.PATCH) {
+    return ".patch";
+  }
+
+  if (type === AssetType.PACKED) {
+    return ".packed.7z";
+  }
+
+  throw new Error(`No file extension for asset type ${type} found`);
 };
