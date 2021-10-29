e904b41561ebd8bf4d42c614ff412998c3dbed1f
xnerhu <xnerhu@gmail.com>
2021-10-29 19:48:05 +0200

    fix: postgres connection from env in updates

--- services/updates/src/config/env.ts
+++ services/updates/src/config/env.ts
@@ -1,4 +1,7 @@
 import Joi from "joi";
+import { config } from "dotenv";
+import { resolve } from "path";
+import { IS_TEST } from "@common/node";
 
 export const SCHEMA_ENV = Joi.object({
   PORT: Joi.number().default(8000),
@@ -10,4 +13,23 @@ export const SCHEMA_ENV = Joi.object({
   RMQ_ENABLED: Joi.boolean().optional().default(true),
   RMQ_URL: Joi.string().required(),
   RMQ_QUEUE: Joi.string().required(),
+  POSTGRES_HOST: Joi.string().required(),
+  POSTGRES_PORT: Joi.number().required(),
+  POSTGRES_DB: Joi.string().required(),
+  POSTGRES_USER: Joi.string().required(),
+  POSTGRES_PASSWORD: Joi.string().required(),
 });
+
+export const getPostgresConfig = () => {
+  if (IS_TEST) {
+    config({ path: resolve(__dirname, "../../.test.env") });
+  }
+
+  return {
+    host: process.env.POSTGRES_HOST as string,
+    port: parseInt(process.env.POSTGRES_HOST as string),
+    dbName: process.env.POSTGRES_DB as string,
+    user: process.env.POSTGRES_USER as string,
+    password: process.env.POSTGRES_PASSWORD as string,
+  };
+};
--- services/updates/src/mikro-orm-config.ts
+++ services/updates/src/mikro-orm-config.ts
@@ -1,16 +1,19 @@
 import { Options } from "@mikro-orm/core";
 
 import { AssetEntity } from "./assets/asset-entity";
+import { getPostgresConfig } from "./config/env";
 import { DistributionEntity } from "./distributions/distribution-entity";
 import { ReleaseEntity } from "./releases/release-entity";
 
+const { host, port, dbName, user, password } = getPostgresConfig();
+
 export const config: Options = {
   type: "postgresql",
-  host: "localhost",
-  port: 5432,
-  dbName: "updates",
-  user: "root",
-  password: "example",
+  host,
+  port,
+  dbName,
+  user,
+  password,
   entities: [ReleaseEntity, DistributionEntity, AssetEntity],
   debug: process.env.NODE_ENV === "development",
   forceUtcTimezone: true,
