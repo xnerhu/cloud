4f29e2957ae7d06578128d9d38adec38e1fc7fbf
xnerhu <xnerhu@gmail.com>
2021-10-30 17:39:11 +0200

    fix: postgres port in updates service

--- services/updates/src/config/env.ts
+++ services/updates/src/config/env.ts
@@ -27,7 +27,7 @@ export const getPostgresConfig = () => {
 
   return {
     host: process.env.POSTGRES_HOST as string,
-    port: parseInt(process.env.POSTGRES_HOST as string),
+    port: parseInt(process.env.POSTGRES_PORT as string),
     dbName: process.env.POSTGRES_DB as string,
     user: process.env.POSTGRES_USER as string,
     password: process.env.POSTGRES_PASSWORD as string,
